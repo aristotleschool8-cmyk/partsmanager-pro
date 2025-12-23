9ae4b55b925c3b55a0773f3da4e12750b4a37f29
Mohammed-DZ1 <the1st.ghost.wizard@gmail.com>
2025-12-23 02:55:12 +0100

    log-purchase-dialog drop down hover effect color fixed

--- src/components/dashboard/log-purchase-dialog.tsx
+++ src/components/dashboard/log-purchase-dialog.tsx
@@ -224,7 +224,7 @@ export function LogPurchaseDialog({ dictionary, onPurchaseAdded }: { dictionary:
                           <div
                             key={option.value}
                             onClick={() => handleSupplierSelect(option)}
-                            className="px-3 py-2 hover:bg-gray-100 cursor-pointer text-sm"
+                            className="px-4 py-3 hover:bg-primary/10 cursor-pointer text-sm transition-colors border-b border-primary/10 last:border-b-0"
                           >
                             {option.label}
                           </div>
